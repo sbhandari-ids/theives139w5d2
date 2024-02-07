c6db89813a3274651848fbb46db2c91e38dc4c66
SB <sbhandari-ids@gmail.com>
2024-02-07 18:29:29 -0500

    Week5 Day2 Pokemon Flask Assignment - code restructuring

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,24 @@
 from flask import render_template, request
 import requests
 from app import app
-
+from .forms import LoginForm
 
 @app.route('/')
 def hello():
    return render_template('home.html')
 
-# @app.route('/user')
-# def user():
-#     return "<h1> Hello there! User </h1>"
+@app.route('/user/<name>')
+def user(name):
+    return f'Hello! {name}'
 
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return f'Hello! {name}'
-
-# @app.route('/login', methods = ['GET','POST'])
-# def login():
-#     if request.method == 'POST':
-#         email = request.form.get('email')
-#         password = request.form.get('password')
-#         return f'{email} {password}'
-#     return render_template('index.html')
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    loginForm = LoginForm()
+    if request.method == 'POST' and loginForm.validate_on_submit():
+        email = loginForm.email.data
+        password = loginForm.password.data 
+        return f'{email} {password}'
+    return render_template('login.html', form = 'loginForm')
 
 def pokemon_info(my_pokemon):
     url = f'https://pokeapi.co/api/v2/pokemon/{my_pokemon}'
@@ -48,7 +44,7 @@ def pokemon():
         pokemon_name_id = request.form.get('pokemon_name_id')
         #print(pokemon_info(pokemon_name_id))
         return pokemon_info(pokemon_name_id) #f'Searched for {pokemon_name_id}'
-    return render_template('pokemon copy.html')
+    return render_template('pokemon.html')
 
 
 
